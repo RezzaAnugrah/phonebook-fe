3e5e9d3879d40aa9261e8b7cb816691a0777906c
Rezza Anugrah Mutiarawan <>
2024-08-15 14:41:55 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,36 @@
 <template>
   <body>
-    <h1>Contact Management</h1>
-    <form id="contactForm" @submit.prevent="create" method="post">
-        <input type="text" id="name" placeholder="Name" required>
-        <input type="email" id="email" placeholder="Email" required>
-        <input type="text" id="phone" placeholder="Phone" required>
+    <h1>PHONEBOOK</h1>
+    <form id="contactForm" @submit.prevent="createItem" method="post">
+        <input type="text" id="name" v-model="data.name" placeholder="Name" required>
+        <input type="email" id="email" v-model="data.email" placeholder="Email" required>
+        <input type="text" id="phoneNo" v-model="data.phoneNo" placeholder="Phone" required>
         <button type="submit">Add Contact</button>
+        &nbsp;
+        <button @click="getListItem">Refresh Data</button>
     </form>
     <table id="contactsTable">
         <thead>
             <tr>
+                <th>ID</th>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Phone</th>
                 <th>Actions</th>
             </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          <tr v-for="(item, index) in items" :key="index">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.email }}</td>
+          <td>{{ item.phoneNo }}</td>
+          <td>
+            <button @click="updateItem(item)">Update</button>
+            <button @click="deleteItem(index)">Delete</button>
+          </td>
+        </tr>
+        </tbody>
     </table>
   </body>
 </template>
@@ -26,15 +40,16 @@ import PhonebookService from "../services/PhonebookService";
 export default {
   data(){
     return {
+      items: [],
       data: {}
     };
   },
 
   methods: {
-    getListContact() {
+    getListItem() {
       PhonebookService.getListContact()
         .then((res) => {
-          console("data : " , res);
+          console.info("data : ", res);
           this.items = res.data.data;
 
         })
@@ -43,7 +58,7 @@ export default {
         });
     },
 
-    create() {
+    createItem() {
       PhonebookService.createContact(this.data)
       .then((res) => {
         this.data = res.data.data;
@@ -54,7 +69,7 @@ export default {
       });
     },
 
-    read() {
+    readItem() {
       PhonebookService.read(this.data)
       .then((res) => {
         this.data = res.data.data;
@@ -66,32 +81,36 @@ export default {
     },
 
 
-    update() {
-      PhonebookService.updateContact(this.data)
+    updateItem(item) {
+      PhonebookService.updateContact(item)
         .then((res) => {
           this.data = res.data.data;
-          this.$router.push({name:'binGateway'});
+          this.getListItem();
         })
         .catch((err) => {
           console.error("err : ", err.message);
         });
     },
 
-    deleteRow(row) {
-      PhonebookService.deleteContact(this.data)
-      .then((res) => {
-        this.data = res.data.data;
-      })
-      .catch((err) => {
-        console.error("err : ", err.message);
-      });
+    deleteItem(row) {
+
+      console.info("info row : ", row);
+
+    //   PhonebookService.deleteContact(this.data)
+    //   .then((res) => {
+    //     this.data = res.data.data;
+    //   })
+    //   .catch((err) => {
+    //     console.error("err : ", err.message);
+    //   });
       
-      this.items.splice(row.originalIndex, 1);
+      this.items.splice(row, 1);
+      //this.getListItem();
     },
   },
 
   mounted() {
-    this.getListContact();
+    this.getListItem();
   },
 };
 </script>
